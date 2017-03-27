2bf51c9245e5c68e6a2930164e8bf742dc13dc9b
Kimsoeon <a1thdjs@naver.com>
2017-03-27 19:58:03 +0900

    #1 xml Layout

---
+++ app/src/main/java/soeonkim/kr/hs/emirim/simplecalculator/MainActivity.java
@@ -0,0 +1,10 @@
+package soeonkim.kr.hs.emirim.simplecalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+    }
+}
